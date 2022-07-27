d9828f76f44dfe93326cfcab1b6b3aa657325bff
EbolaSpreadn247 <lucario2907@gmail.com>
2022-07-27 11:53:32 +0100

    testing aspx connection
    Wanna see if github pages hosting gives me easy to find urls to use GET on in order to bypass my CORS errors. Please work! can't test it till i commit :((

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var textarea_ids = ["player_name","slime"]
-;function check_marks(){
+var textarea_ids = ["player_name","slime"];
+function check_marks(){
     for(var i = 0; i < textarea_ids.length; i++){
         let textarea = document.getElementById("textarea_"+ textarea_ids[i]);
         if(textarea.value != ""){
@@ -10,4 +10,15 @@ var textarea_ids = ["player_name","slime"]
             document.getElementById("img_"+ textarea_ids[i]).src = "images/slime1.png";
         }
     }
+}
+
+function Enter_Enter(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://ebolaspreadn247.github.io/repo-depo/ajax_info.txt", true);
+    xhttp.send();
 }
